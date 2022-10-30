02e1547bf40767921256450e4f3010b809acdff0
huyphamfc <huyphamfc@gmail.com>
2022-10-30 10:37:18 +0700

    connect MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 dotenv.config();
+const { MONGODB_URI } = process.env;
 const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('Connect MongoDB successfully.'));
 
 app.listen(PORT, () => console.log(`Sever running on port ${PORT}.`));
